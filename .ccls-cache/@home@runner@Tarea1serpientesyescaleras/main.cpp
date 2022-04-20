4552c0adc6ea5d0ca9ff9a4d713649eda4c5a5a9
Francisco Salas <88806099+Heladoman03@users.noreply.github.com>
2022-04-20 01:34:53 +0000

    Cambiar nombre de snakes a main

--- .ccls-cache/@home@runner@Tarea1serpientesyescaleras/main.cpp
+++ .ccls-cache/@home@runner@Tarea1serpientesyescaleras/main.cpp
@@ -1,12 +1,98 @@
 //Francisco Salas Porras A01177893
+//Iván López A01284875
+//Gabriel Ruiz Miller A00835179
 
 #include <iostream>
-#include <cctype>
 #include <stdlib.h>
+#include <chrono>
+#include <thread>
 #include "Continue.h"
+#include "CDado.h"
+#include "MyGame.h"
 using namespace std;
 
+//class MyGame{
+//	private:
+//	
+//	public:
+//};
+
+//class Player{
+//	private:
+//	int num;
+//	int pos;
+//
+//	public:
+//	Player();
+//	Player(int);
+//	~Player();
+//	string numplayer();
+//	int throwdice();
+//	
+//};
+
+
+
+// class Dice{
+//
+//};
+
+//class Tile{
+//	private:
+//	string tablero;
+//	public:
+//	Tile();
+//	Tile(string);
+//	~Tile();
+//};
+//
+//Tile::Tile(){
+//	
+//}
+//
+//Tile::Tile(string _s){
+//	
+//};
+
+//class Serpiente : public Tile{
+//	private:
+//	int punish;
+//	public:
+//	Serpiente();
+//	~Serpiente();
+//	int setpunish();
+//	int getpunish();
+//
+//};
+
+//class Ladder : public Tile{
+//	private:
+//	int reward;
+//	public:
+//	Escalera();
+//	~Escalera();
+//	int setreward();
+//	int getreward();
+//};
+
+//class Tablero{
+//	private:
+//	string tablero;
+//
+//	public:
+//	Tablero();
+//	~Tablero();
+//	
+//};
+			// for i 2:29
+
 int main() {
+	MyGame snakes;
+	
+	snakes.startGame();
+
 	Continue continuar;
-	cout << continuar.getcon() << endl;
+	CDado tiraDado;
+	continuar.setcon();
+	cout << tiraDado.GetValorDado() << endl;
 }
